a0e163c2ecbcf0451133288c1ef0d2e4030a091a
Francesc Rosas <francescrosasbosque@gmail.com>
2017-08-21 11:51:25 +0200

    Start web server ASAP

--- src/index.js
+++ src/index.js
@@ -19,18 +19,25 @@ const getCalendar = async () => {
       url: episode.program.url
     });
   });  
+  console.log('Episodes retrieved');
   return calendar;
 }
 
 (async () => {
-  let calendar;
-  await (async function _updateCalendar() {
-    calendar = await getCalendar();
+  let whenCalendar;
+  (async function _updateCalendar() {
+    whenCalendar = getCalendar();
+    // Avoid potential calls overlapping by not scheduling the next run until the
+    // current one is over.
+    await whenCalendar;
     setTimeout(_updateCalendar, 10 /* min */ * 60 * 1000);
   })();
+
+  // Note we start the server without waiting for the calendar to be obtained.
+  // Instead, we use its promise to serve it once it resolves.
   console.log('Starting server...');
   const app = express();
-  app.get('/ics', (req, res) => calendar.serve(res))
+  app.get('/ics', async (req, res) => (await whenCalendar).serve(res))
   await util.promisify(app.listen.bind(app))(process.env.PORT || 80);
-  console.log('Done');
+  console.log('Server started');
 })();
