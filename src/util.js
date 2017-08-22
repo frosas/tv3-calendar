c0311017c40e107fe8aa4c228ad1d66df54168c4
Francesc Rosas <francescrosasbosque@gmail.com>
2017-08-22 17:31:59 +0200

    Clarify retryify() works asynchronously
    It would be possible to support working synchronously as well but it
    wouldn't make much sense.

--- src/util.js
+++ src/util.js
@@ -2,6 +2,9 @@ const util = exports;
 
 util.delay = interval => new Promise(resolve => setTimeout(resolve, interval));
 
+/**
+ * @return {Promise}
+ */
 util.retryify = func => {
   return (...args) => {
     return (async function retriedFunc(params = {args, attempt: 1}) {
