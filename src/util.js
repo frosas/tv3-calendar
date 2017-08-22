6e071011ef91402a1c42201947573c1bd702fd96
Francesc Rosas <francescrosasbosque@gmail.com>
2017-08-22 12:58:17 +0200

    Promise -> async/await

--- src/util.js
+++ src/util.js
@@ -2,11 +2,12 @@ exports.delay = interval => new Promise(resolve => setTimeout(resolve, interval)
 
 exports.retryify = func => {
   return (...args) => {
-    return (function retriedFunc(params = {args, attempt: 1}) {
-      return Promise
-        .resolve()
-        .then(() => func(params))
-        .catch(error => retriedFunc({...params, error, attempt: params.attempt + 1}));
+    return (async function retriedFunc(params = {args, attempt: 1}) {
+      try {
+        return await func(params);
+      } catch (error) {
+        return retriedFunc({...params, error, attempt: params.attempt + 1});
+      }
     })();
   };
 };
